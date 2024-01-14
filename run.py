33fb728320f8580682781bf4514be4dc609a5540
JorgenDIF <jorgenjonsson80@gmail.com>
2024-01-14 10:26:02 +0100

    Add game introduction and basic functions

--- run.py
+++ run.py
@@ -1,4 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Hello World!")
+import random
+import time
+import os
+
+
+def main():
+    print("Welcome to Track Quest!")
+    print("This game will take you on journeys across the world.")
+    print("Its built from the concept of the swedish tv show 'På spåret'.")
+    print("You will be given clues of where you are going.")
+    print("The game will be played in rounds.")
+    print("Each question has 5 rounds.")
+    print("The rounds goes from 10 points to 2 points.")
+    player_name = input("What is your name? ")
+    print("Welcome " + player_name + "!")
+
+
+
+main()
+
+def game_instructions():
+    pass
+
+
+
+def game_start():
+    pass
+
+def show_question():
+    pass    
+
+
