ae012101b1975abfb6faf2d1839e33f9ccd41abd
JorgenDIF <jorgenjonsson80@gmail.com>
2024-01-14 19:33:15 +0100

    Add slow print function and update welcome messages

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ The game is built with inspiration from the youtube tutorial
 import random
 import time
 import os
+import sys
 
 # Global variables
 score = 0
@@ -33,6 +34,20 @@ questions = [
 ]
 
 
+"""
+ Function to print text with a delay between each character. 
+ Credit to: https://replit.com/talk/learn/The-Slow-Print/44741
+ """
+
+
+def delprint(text, delay_time): 
+    for character in text:      
+        sys.stdout.write(character) 
+        sys.stdout.flush()
+        time.sleep(delay_time)
+    print("")
+
+  
 # Clears the screen based on the user's operating system.
 def clear():
     # for Windows
@@ -44,12 +59,12 @@ def clear():
 
 
 def main():
-    print("Welcome to Track Quest!")
-    print("This game concept is from the swedish TV-show 'På spåret'")
-    print("The goal is to guess the city from the clues given")
-    print("The game will be played in 5 rounds.")
-    print("The rounds goes from 10 points to 2 points.")
-    print("Enter your name to continue")
+    delprint("Welcome to Track Quest!", 0.1)
+    delprint("This game concept is from the swedish TV-show 'På spåret'", 0.1)
+    delprint("The goal is to guess the city from the clues given", 0.1)
+    delprint("The game will be played in 5 rounds.", 0.1)
+    delprint("The rounds goes from 10 points to 2 points.", 0.1)
+    delprint("Enter your name to continue", 0.1)
 
     while True:
         player_name = input("Please enter your preferred username: ")
@@ -65,9 +80,10 @@ main()
 
 def game_start():
     clear()
-    print("Where are we heading?") 
+
+    delprint("Where are we heading?", 0.1) 
     for question in questions:
-        print(question["question"][0]["text"])
+        delprint(question["question"][0]["text"], 0.1)
 
 
 game_start()
